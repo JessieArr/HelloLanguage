a496218b45f6ded661eaa95cc12d075ad58643ed
Shawn Morrison <smorrison1986@gmail.com>
2015-12-09 22:47:13 -0600

    Adding Hello World in F#!

---
+++ Functional Languages/F#/HelloWorld/HelloWorld/Program.fs
@@ -0,0 +1,10 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+open System
+
+[<EntryPoint>]
+let main argv = 
+    printfn "Hello, World!"
+    Console.ReadLine() |> ignore
+    0 // return an integer exit code
